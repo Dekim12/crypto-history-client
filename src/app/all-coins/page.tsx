83a8e9b3045ffdca19765adad75e4b036290714f
Siarhei Zykau <zykau_s@mail.ru>
2023-08-28 23:04:56 +0300

    feat: add the repo

---
+++ src/app/all-coins/page.tsx
@@ -0,0 +1,7 @@
+export default function AllCoins() {
+  return (
+    <div>
+      <h1>All Coins</h1>
+    </div>
+  );
+}
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,10 @@
+export default function AppHeader() {
+  return (
+    <header className='align-it bg t flex h-12 items-center justify-between bg-gray-700 px-4 text-neutral-200'>
+      <a className='text-xl' href='#'>
+        CRYPTO
+      </a>
+      <button className='p-1 text-lg'>log out</button>
+    </header>
+  );
+}
